4ae01344a9b0159649550f6c580db02a03d2a73d
Peter Dodds <peter@pddds.com>
2011-03-05 20:13:44 +0000

    exposed window width and height to sub files

--- main.lua
+++ main.lua
@@ -10,22 +10,23 @@ function shutdown()
     love.event.push('q')
 end
 
+window = { width = 100, height = 100 }
+
 -- get largest display from possible fullscreen modes
 local modes = love.graphics.getModes()
-local best = { w = 0, h = 0 }
 for i = 1, #modes do
     local mode = modes[i]
     local width = mode.width
     local height = mode.height
 
-    if width > best.w and width > height then
-        best.w = width
-        best.h = height
+    if width > window.width and width > height then
+        window.width = width
+        window.height = height
     end
 end
 
 -- set to fullscreen and set title
-local setModeOk = love.graphics.setMode(best.w, best.h, true, true, 0)
+local setModeOk = love.graphics.setMode(window.width, window.height, true, true, 0)
 if not setModeOk then shutdown() end
 
 love.graphics.setCaption("My First Love")
